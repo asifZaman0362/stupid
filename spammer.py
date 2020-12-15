fd5606cecafba7d95afde732d31f99caa66f27af
asifZaman0362 <zero362001@gmail.com>
2020-12-15 21:57:15 +0530

    improvements

--- spammer.py
+++ spammer.py
@@ -7,20 +7,21 @@ def send_message(clipboard=True, mssg=""):
 	if clipboard:
 		pyautogui.hotkey("ctrlleft","v")
 		pyautogui.hotkey("return")
-	else:
-		# whatever
 
 
 
 def start():
+	t = 0
 	while True:
 		time.sleep(0.01)
 		if keyboard.is_pressed("p"):
 			break;
 	while(True):
-		time.sleep(0.2)
-		send_message()
-		if keyboard.is_pressed("p"):
+		time.sleep(0.01)
+		t += 1
+		if t % 10 == 0:
+			send_message()
+		if keyboard.is_pressed("q"):
 			break
 
 
